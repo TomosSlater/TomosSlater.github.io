a27f15f34fe942c35645508efba29fde07780684
Tomos Slater <Tomos.Slater@softwire.com>
2021-02-26 11:41:48 +0000

    Improve key event handling

--- script.js
+++ script.js
@@ -2,19 +2,6 @@ var img = document.getElementById("moveTest");
 var rows = ["SRow", "ARow", "BRow", "CRow", "DRow", "ERow", "FRow"]
 var currentRow = rows.indexOf(img.parentElement.id);
 
-document.onkeydown = moveRow;
-
-function moveRow(e){
-    console.log("KEYDOWN!!" + e.KeyCode);
-    e = e || window.event;
-
-    if(e.KeyCode == '38' && currentRow > 0){
-        currentRow--;
-        document.getElementById(rows[currentRow]).appendChild(img);
-        console.log("KEYDOWN(38)")
-    } else if (e.KeyCode == '37' && currentRow < rows.length){
-        currentRow++;
-        document.getElementById(rows[currentRow]).appendChild(img);
-        console.log("KEYDOWN(37)")
-    }
-}
+document.addEventListener('keydown', function(event) {
+    console.log("KEYDOWN: " + event.key);
+});
