c365357b14cf74c635da4ac3e192589b3886b8e6
Tomos Slater <Tomos.Slater@softwire.com>
2021-02-26 11:43:26 +0000

    Add functionality to move image when key pressed

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ var currentRow = rows.indexOf(img.parentElement.id);
 
 document.addEventListener('keydown', function(event) {
     console.log("KEYDOWN: " + event.key);
+
+    if(event.key === 'ArrowUp' && currentRow > 0){
+        currentRow--;
+        document.getElementById(rows[currentRow]).appendChild(img);
+    } else if (event.key === 'ArrowDown' && currentRow < rows.length){
+        currentRow++;
+        document.getElementById(rows[currentRow]).appendChild(img);
+    }
 });
